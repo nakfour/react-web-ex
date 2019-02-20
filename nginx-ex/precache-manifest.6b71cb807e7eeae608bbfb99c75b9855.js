398f420b51b824a2f553ff6f0ae5354c4ab9e89d
Chris Chase <cchase@redhat.com>
2019-02-20 16:57:56 -0500

    Add built react app to nginx-ex .

---
+++ nginx-ex/precache-manifest.6b71cb807e7eeae608bbfb99c75b9855.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9e2893dbc4f4c43e841a",
+    "url": "/static/js/main.9e2893db.chunk.js"
+  },
+  {
+    "revision": "0e493f63e1ac10c82d37",
+    "url": "/static/js/1.0e493f63.chunk.js"
+  },
+  {
+    "revision": "9e2893dbc4f4c43e841a",
+    "url": "/static/css/main.344a7517.chunk.css"
+  },
+  {
+    "revision": "622690f7a30be746d549a595a50f33aa",
+    "url": "/index.html"
+  }
+];

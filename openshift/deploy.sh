d952a9126ba4cb91af230502fa66cd581f9da135
Juana Nakfour <nakfour>
2019-03-19 13:54:34 -0500

    deleting everyhting buy ngnix

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -3,8 +3,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-oc process -f ${DIR}/nodejs-server.yml | oc create -f -
-oc process -f ${DIR}/react-web-app.yml | oc create -f -
+#oc process -f ${DIR}/nodejs-server.yml | oc create -f -
+#oc process -f ${DIR}/react-web-app.yml | oc create -f -
 oc process -f ${DIR}/nginx-ex.yml | oc create -f -
 
 
--- openshift/undeploy.sh
+++ openshift/undeploy.sh
@@ -4,5 +4,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 oc process -f ${DIR}/nginx-ex.yml | oc delete -f -
-oc process -f ${DIR}/react-web-app.yml | oc delete -f -
-oc process -f ${DIR}/nodejs-server.yml | oc delete -f -
+#oc process -f ${DIR}/react-web-app.yml | oc delete -f -
+#oc process -f ${DIR}/nodejs-server.yml | oc delete -f -
